2397833d01bfe924b258b46ad3bc4f83bd1671c0
Pravenkaia <pravlen@rukzak.ru>
2019-11-24 12:05:33 +0300

    bug: replace config

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
-const DB_DRIVER  = 'mysql';
-const DB_HOST    = 'localhost';
-const DB_USER    = 'lenkatoysg_task';
-const DB_PASS    = 'jxykt75EUI++';
-const DB_NAME    = 'lenkatoysg_task';
+const DB_DRIVER = 'mysql';
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASS = '';
+const DB_NAME = 'task';
 const DB_CHARSET = 'utf8';
 
 const DB_OPTIONS = [
